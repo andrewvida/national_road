78d5ed406000164669d164ef0aac100377c1e7d6
Andrew Vida <andy@testdouble.com>
2014-10-15 09:43:35 -0400

    adding a new rails project

---
+++ national_road/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NationalRoad::Application.config.secret_key_base = '0048f75a704ce4566eb61ba59db4e4afd717ea06ebb07418fa7ba77541679eae1059e62b6cd5abd4f416f2f4e97490e5d90c060f9d7d665e7029315696715c87'
---
+++ national_road/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NationalRoad::Application.config.session_store :cookie_store, key: '_national_road_session'
